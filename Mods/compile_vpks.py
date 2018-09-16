769279c8a257268ad6c46d04aeaaafe7adadbe1f
iven-yang <risingsky1@gmail.com>
2018-09-16 16:37:36 -0400

    buff melee weapons + Hunting Rifle + Sniper

--- Mods/compile_vpks.py
+++ Mods/compile_vpks.py
@@ -6,7 +6,7 @@ filenames= os.listdir(".")
 results = []
 for filename in filenames:
     # Get all folders except the git folder and vpk output folder
-    if os.path.isdir(os.path.join(os.path.abspath("."), filename)) and filename != '.git':
+    if os.path.isdir(os.path.join(os.path.abspath("."), filename)):
         results.append(filename)
 
 vpk_dir = r'C:\Program Files (x86)\Steam\steamapps\common\Left 4 Dead 2\bin\vpk.exe'
